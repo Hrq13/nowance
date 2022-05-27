cd0c8a54a9a674c7ed8fe381cd59131ee2225012
Henrique F. Miranda <h.miranda20@outlook.com>
2022-05-26 22:14:12 -0300

    Create NavBar component, add NavBarLink type, and remove unnecessary code

---
+++ src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Nowance</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              data-bs-display="static"
+            >
+              Menu
+            </a>
+            <ul
+              class="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdown"
+            >
+              <li v-for="link in props.links" :key="link.path">
+                <a class="dropdown-item" href="#">{{ link.label }}</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import type { Link as NavBarLink } from "@/types/NavBar.types";
+import type { PropType } from "vue";
+
+const props = defineProps({
+  links: {
+    type: Array as PropType<NavBarLink[]>,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+nav.navbar {
+  z-index: 1000;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <template>
   <main>
-    <HelloWorld msg="YEAH!!" />
+    <h1>BODY</h1>
   </main>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+h1 {
+  display: inline-block;
+  position: absolute;
+  bottom: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: rotate 3s linear infinite;
+}
+
+@keyframes rotate {
+  from {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  to {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+</style>
