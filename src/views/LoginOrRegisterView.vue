65144a8cb92f3b807c2d66603afd326c4830b0e7
Henrique F. Miranda <h.miranda20@outlook.com>
2022-05-29 14:20:51 -0300

    Remove horizontal padding of LoginForm in very small screens

--- src/views/LoginOrRegisterView.vue
+++ src/views/LoginOrRegisterView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="container row position-absolute top-50 start-50 translate-middle m-0"
+    class="container row position-absolute top-50 start-50 translate-middle m-0 px-0 px-sm-4"
   >
     <login-form
       class="col-sm-10 col-md-8 col-lg-6"
