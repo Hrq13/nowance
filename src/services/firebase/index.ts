bf536001c0856c77c894785562bddb6da42a1448
Henrique F. Miranda <h.miranda20@outlook.com>
2022-06-28 21:27:51 -0300

    Create Firebase and Firestore service files

---
+++ src/services/firebase/index.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9kQtiCl64M9RlBb84T5glo6txgKtMqKE",
+  authDomain: "nowance-enterprise.firebaseapp.com",
+  projectId: "nowance-enterprise",
+  storageBucket: "nowance-enterprise.appspot.com",
+  messagingSenderId: "832629031515",
+  appId: "1:832629031515:web:9ab55d405071db9daf357e",
+  measurementId: "G-SLYP81VD4Z",
+};
+
+export default initializeApp(firebaseConfig);
---
+++ src/services/firestore/firestore.ts
@@ -0,0 +1,4 @@
+import { getFirestore } from "firebase/firestore";
+import App from "@/services/firebase";
+
+export default getFirestore(App);
